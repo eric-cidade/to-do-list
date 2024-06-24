7576e94036c9a76b860a905a39252f35e9b21bfc
Eric <ericmathias2012@gmail.com>
2024-06-24 12:21:49 -0300

    task done funcionality implemented

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,11 +1,30 @@
 import styles from './Task.module.css'
 import { Trash } from 'phosphor-react';
+import { Check } from 'phosphor-react';
+import { useState} from 'react';
+
 export function Task() {
-    return (
-        <div className={styles.task}>
-            <button className={styles.checkbox} />
+    const [isChecked, setIsChecked] = useState(false);
+    function handleCheckBoxClick() {
+        isChecked ? setIsChecked(false) : setIsChecked(true)
+    }
+    if(!isChecked)
+    {
+        return (
+            <div className={styles.task}>
+                <button className={styles.checkBoxEmpty} onClick={handleCheckBoxClick}/>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis adipisci, cupiditate numquam fuga libero voluptates est pariatur tempora fugiat quia.</p>
+                <button className={styles.trash}><Trash size={24}/></button>
+            </div>
+        );
+    }
+    else return (
+        <div className={styles.taskDone}>
+            <button className={styles.checkBoxFilled} onClick={handleCheckBoxClick}>
+                <Check />
+            </button>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis adipisci, cupiditate numquam fuga libero voluptates est pariatur tempora fugiat quia.</p>
             <button className={styles.trash}><Trash size={24}/></button>
         </div>
-    );
+    )
 }
